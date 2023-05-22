bc4bc12be6084fbdd298a6bd3ceadb5e104a1c41
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-05-22 12:51:11 +0200

    login and loginPage init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import React from "react";
-import { Login } from "./Routes.js";
+import { LoginPage } from "./Routes.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
