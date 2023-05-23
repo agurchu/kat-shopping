7e49f13e89a1e8060fb83616f440c7a0c590e66b
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-05-23 21:07:03 +0200

    activation for mail

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -39,13 +39,13 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-tutorial-cefl.vercel.app/activation/${activationToken}`;
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
 
     try {
       await sendMail({
         email: user.email,
         subject: "Activate your account",
-        message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+        message: `Hello ${user.fullName}, please click on the link to activate your account: ${activationUrl}`,
       });
       res.status(201).json({
         success: true,
