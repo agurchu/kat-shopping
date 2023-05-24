b7f08d0cdf308a478fe51536c92575f942b2498b
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-05-24 19:26:23 +0200

    redux installing

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -15,7 +15,11 @@ export default function Login() {
     e.preventDefault();
 
     await axios
-      .post(`${server}/user/login-user`, { email, password })
+      .post(
+        `${server}/user/login-user`,
+        { email, password },
+        { withCredentials: true }
+      )
       .then((res) => {
         toast.success("Login Successful!");
         navigate("/");
