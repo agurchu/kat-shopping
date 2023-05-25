94071f35666e36295a1a944647d57282b62d0a9d
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-05-25 20:20:41 +0200

    header.jsx seller &categories init

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { productData } from "../../static/data";
 import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosArrowForward } from "react-icons/io";
+import { BiMenuAltLeft } from "react-icons/bi";
 
 export default function Header() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchData, setSearchData] = useState("");
+  const [active, setActive] = useState(false);
 
   const handleSearchChange = (e) => {
     const term = e.target.value;
@@ -17,51 +20,82 @@ export default function Header() {
     setSearchData(filteredProducts);
   };
 
-  return (
-    <div className="section">
-      <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
-        <div>
-          <Link to="/">
-            <img className="h-12" src="kj_dev_logo.png" alt="logo" />
-          </Link>
-        </div>
-        {/* search box */}
-        <div className="relative w-[50%]">
-          <input
-            type="text"
-            placeholder="Search Product"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            className="border-orange-600 rounded-md w-full border-2 px-2 h-10"
-          />
-          <AiOutlineSearch
-            size={30}
-            className="absolute right-2 top-1.5 cursor-pointer"
-          />
-          {searchData && searchData.length !== 0 ? (
-            <div className="absolute min-h-[30hv] bg-slate-50 shadow-sm-2 z-[9] p-4">
-              {searchData &&
-                searchData.map((i, index) => {
-                  const d = i.name;
+  window.addEventListener("scroll", () => {
+    if (window.screenY > 70) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  });
 
-                  const product_name = d.replace(/\s+/g, "-");
-                  return (
-                    <Link key={index} to={`/product/${product_name}`}>
-                      <div className="w-full flex items-start-py-3">
-                        <img
-                          src={i.image_Url[0].url}
-                          alt=""
-                          className="w-[40px] h-[40px] mr-[10px]"
-                        />
-                        <h1>{i.name}</h1>
-                      </div>
-                    </Link>
-                  );
-                })}
-            </div>
-          ) : null}
+  return (
+    <>
+      <div className="section">
+        <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
+          <div>
+            <Link to="/">
+              <img className="h-12" src="kj_dev_logo.png" alt="logo" />
+            </Link>
+          </div>
+          {/* search box */}
+          <div className="relative w-[50%]">
+            <input
+              type="text"
+              placeholder="Search Product"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="border-orange-600 rounded-md w-full border-2 px-2 h-10"
+            />
+            <AiOutlineSearch
+              size={30}
+              className="absolute right-2 top-1.5 cursor-pointer"
+            />
+            {searchData && searchData.length !== 0 ? (
+              <div className="absolute min-h-[30hv] bg-slate-50 shadow-sm-2 z-[9] p-4">
+                {searchData &&
+                  searchData.map((i, index) => {
+                    const d = i.name;
+
+                    const product_name = d.replace(/\s+/g, "-");
+                    return (
+                      <Link key={index} to={`/product/${product_name}`}>
+                        <div className="w-full flex items-start-py-3">
+                          <img
+                            src={i.image_Url[0].url}
+                            alt=""
+                            className="w-[40px] h-[40px] mr-[10px]"
+                          />
+                          <h1>{i.name}</h1>
+                        </div>
+                      </Link>
+                    );
+                  })}
+              </div>
+            ) : null}
+          </div>
+          <div className="button">
+            <Link to="/seller">
+              <h1 className="text-white flex items-center">
+                Become Seller <IoIosArrowForward className="ml-1" />{" "}
+              </h1>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div
+        className={`${
+          active === true ? " shadow-sm fixed top-0 left-0 z-10" : null
+        } transition hidden 800px:flex items-center justify-between w-full bg-orange-700 h-[70px]`}
+      >
+        <div className="section relative normalFlex justify-between">
+          {/* categories */}
+          <div>
+            <div className="relative h-16 mt-2.5 w-[270px]  1000px:block">
+              <BiMenuAltLeft size={30} className="absolute top-3 left-2" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
