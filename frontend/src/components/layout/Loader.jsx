66dafc5682cab12732bde5cc6708f1e212c04e0a
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-06-04 17:19:45 +0200

    24151-ecommerce-animation.json & Loader

--- frontend/src/components/layout/Loader.jsx
+++ frontend/src/components/layout/Loader.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../../Assests/animations/24151-ecommerce-animation.json";
 
 export default function Loader() {
-  return <div>Loader</div>;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Lottie options={defaultOptions} width={300} height={300} />
+    </div>
+  );
 }
