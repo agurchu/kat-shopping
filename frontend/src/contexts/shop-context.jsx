6dff0c678e50cbea58ac5f8ad07c141fb759752c
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-06-21 15:42:41 +0200

    shop-context

---
+++ frontend/src/contexts/shop-context.jsx
@@ -0,0 +1,55 @@
+import React, { createContext, useState } from "react";
+
+import { productData } from "../static/data";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < productData.length + 1; i++) {
+    cart[i] = 0;
+  }
+
+  return cart;
+};
+
+export default function ShopContextProvider({ children }) {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const getTotalAmount = () => {
+    let totalAmount = 0;
+    for (let item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = productData.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
+
+  const handleAddToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const handleRemoveFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const updateItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
+
+  const contextValue = {
+    updateItemCount,
+    cartItems,
+    handleAddToCart,
+    handleRemoveFromCart,
+    getTotalAmount,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>{children}</ShopContext.Provider>
+  );
+}
